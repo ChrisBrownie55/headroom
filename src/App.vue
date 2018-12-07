83310978699d948d6792effef4722089f16ff06a
Christopher Brown <chbphone55@gmail.com>
2018-12-07 15:32:37 -0700

    transition name setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <loading v-if="!ready" />
-    <router-view v-else />
+    <transition
+      :name="transitionName"
+      mode="out-in"
+    >
+      <loading v-if="!ready" />
+      <router-view v-else />
+    </transition>
   </div>
 </template>
 
@@ -9,7 +14,27 @@
   import { mapGetters } from 'vuex';
   import Loading from './components/Loading.vue';
 
+  const DEFAULT_TRANSITION = 'fade';
+
   export default {
+    data() {
+      transitionName: DEFAULT_TRANSITION
+    },
+    created() {
+      this.$router.beforeEach((to, from, next) => {
+        let transitionName = to.meta.transitionName || from.meta.transitionName;
+
+        if (transitionName === 'slide') {
+          const toDepth = to.split('/').length;
+          const fromDepth = to.split('/').length;
+          transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left';
+        }
+
+        this.transitionName = transitionName || DEFAULT_TRANSITION;
+
+        next();
+      });
+    },
     computed: {
       ...mapGetters(['ready']),
     },
@@ -25,8 +50,6 @@
   }
 
   #app {
-    --vue-green: #42b983;
-
     position: relative;
 
     min-height: 100vh;
