a7725490a4830a4a6ec34b817554500d49727b2b
Christopher Brown <chbphone55@gmail.com>
2018-12-07 15:10:21 -0700

    show loading

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,11 +1,18 @@
 <template>
-  
+  <p class="loading">Loading...</p>
 </template>
 <script>
-export default {
-  name: 'loading'
-}
+  export default {
+    name: "loading"
+  };
 </script>
 <style lang="scss">
+  .loading {
+    position: absolute;
+    top: 50%;
 
+    color: #42b983;
+
+    transform: translateY(-50%);
+  }
 </style>
--- src/store.js
+++ src/store.js
@@ -5,7 +5,11 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    user: undefined,
+  },
+  getters: {
+    ready: state => state.user !== undefined,
+    isLoggedIn: state => !!state.user,
   },
   mutations: {
 
