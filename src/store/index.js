0a3f64ecd6a9522248ace51eb7e5986ba5566fa0
Christopher Brown <chbphone55@gmail.com>
2018-12-07 21:19:30 -0700

    migrate notify to global

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,32 @@ import modules from './modules';
 
 Vue.use(Vuex);
 
+let notificationID = 0;
+
 const store = new Vuex.Store({
   modules,
   // Enable strict mode in development to get a warning
   // when mutating state outside of a mutation.
   // https://vuex.vuejs.org/guide/strict.html
   strict: process.env.NODE_ENV !== 'production',
+  mutations: {
+    ADD_NOTIFICATION(state, notification) {
+      state.notifications.push(notification);
+    },
+    REMOVE_NOTIFICATION(state, id) {
+      state.notifications = state.notifications.filter(notification => notification.id !== id);
+    },
+  },
+  actions: {
+    notify({ commit, dispatch }, notification) {
+      const id = notificationID++;
+      commit('ADD_NOTIFICATION', {
+        ...notification,
+        remove: () => dispatch('REMOVE_NOTIFICATION', id),
+        id,
+      });
+    },
+  },
 });
 
 // Automatically run the `init` action for every module,
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -23,29 +23,26 @@ export const actions = {
   setRerouteTo({ commit }, route) {
     commit('SET_REROUTE', route);
   },
-  notify({ commit }, notification) {
-
-  },
-  async login({ commit, dispatch }) {
+  async login({ commit, dispatch, rootDispatch }) {
     try {
       const { user } = await auth.signInWithPopup(provider);
       commit('SET_USER', user);
     } catch (error) {
-      dispatch('notify', {
+      rootDispatch('notify', {
         message: 'An error occurred, please try again.',
         button: {
           text: 'Try again',
-          onClick: () => dispatch('logout'),
+          onClick: () => dispatch('login'),
         },
       });
     }
   },
-  async logout({ commit, dispatch }) {
+  async logout({ commit, dispatch, rootDispatch }) {
     try {
       await auth.signOut();
       commit('SET_USER', null);
     } catch (error) {
-      dispatch('notify', {
+      rootDispatch('notify', {
         message: 'An error occurred, please try again.',
         button: {
           text: 'Try again',
