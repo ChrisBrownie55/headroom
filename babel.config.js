c5f95ca6423bc1795b0067aab507faf3b7dad599
Christopher Brown <chbphone55@gmail.com>
2018-12-19 23:26:02 +0000

    lot o changey, mostly installs

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,9 @@ module.exports = api => {
   const isProduction = api.cache(() => process.env.NODE_ENV === "production");
 
   const presets = ['@babel/env'];
-  const plugins = [];
+  const plugins = [
+    '@babel/plugin-transform-react-display-name',
+  ];
 
   if (isProduction) {
     plugins.push(["htm", {
