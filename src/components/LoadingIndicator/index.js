f3caa0fa454bd6a6775565a4778cfff9a16176f1
Christopher Brown <chbphone55@gmail.com>
2019-01-06 11:11:16 +0000

    allow custom styles for loading indicator

--- src/components/LoadingIndicator/index.js
+++ src/components/LoadingIndicator/index.js
@@ -4,24 +4,42 @@ import './style.css';
 const LoadingIndicator = ({
   width,
   color,
+  completeColor,
+  backgroundColor,
+
   label = 'Loading...',
   complete = false,
   completeLabel = 'Finished loading!',
   className,
   ...props
 }) => html`
-  <div>
-    <div className=${classNames('loading-indicator', className)} ...${props}></div>
-    <span>${complete ? completeLabel : label}</span>
+  <div
+    className=${classNames('loading-indicator', { 'loading-indicator__bar--complete': complete }, className)}
+    style=${{
+    '--width': width,
+    '--color': color,
+    '--complete': completeColor,
+    '--background': backgroundColor
+  }}
+    ...${props}
+  >
+    <div className=loading-indicator__bar></div>
+    <span className=loading-indicator__label>${complete ? completeLabel : label}</span>
   </div>
 `;
 
 LoadingIndicator.propTypes = {
+  // style
   width: PropTypes.string,
   color: PropTypes.string,
+  completeColor: PropTypes.string,
+  backgroundColor: PropTypes.string,
+
+  // functionality
   label: PropTypes.string,
   complete: PropTypes.bool,
-  completeLabel: PropTypes.string
+  completeLabel: PropTypes.string,
+  className: PropTypes.any
 };
 
-export default LoadingIndicator;
+export default memo(LoadingIndicator);
